d490d49e1d1d965197775631b23d48c27efae81a
Harshad Nawathe <harshadn@thoughtworks.com>
2020-12-21 12:41:10 +0530

    Harshad - Session 2: Control structure

--- src/main/kotlin/com/thoughtworks/counter/Application.kt
+++ src/main/kotlin/com/thoughtworks/counter/Application.kt
@@ -1,11 +1,32 @@
 package com.thoughtworks.counter
 
+import kotlin.random.Random
+
 
 fun main() {
-    print("Enter you name: ")
-    val userName: String? = readLine()
+    val secret = Random.nextInt(1, 101)
 
-    println("Hello, $userName${"!".repeat(excitementLevel(userName))}")
+    var count = 0
+    var isDone = false
+    while (count < 10 && !isDone) {
+        println("Guess the number!")
+        val guess: Int? = readLine()?.toIntOrNull()
+
+        if (guess != null) {
+            when {
+                secret < guess -> println("guess is too large!")
+                secret > guess -> println("guess is too small!")
+                else -> {
+                    println("You guessed it!!!!!")
+                    isDone = true
+                }
+            }
+        } else {
+            println("Enter a valid number")
+        }
+        count++
+        if(count == 10) {
+            println("Game over.")
+        }
+    }
 }
-
-fun excitementLevel(name: String?) = name?.length ?: 0
