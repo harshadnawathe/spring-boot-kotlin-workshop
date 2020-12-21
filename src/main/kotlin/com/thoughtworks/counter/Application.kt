253dea6df0057b043158a8009dda76e72f575ef8
Harshad Nawathe <harshadn@thoughtworks.com>
2020-12-21 11:07:51 +0530

    Harshad - Session 1

--- src/main/kotlin/com/thoughtworks/counter/Application.kt
+++ src/main/kotlin/com/thoughtworks/counter/Application.kt
@@ -2,5 +2,10 @@ package com.thoughtworks.counter
 
 
 fun main() {
-    println("Hello, world!")
+    print("Enter you name: ")
+    val userName: String? = readLine()
+
+    println("Hello, $userName${"!".repeat(excitementLevel(userName))}")
 }
+
+fun excitementLevel(name: String?) = name?.length ?: 0
