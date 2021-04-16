439b4f8c979d23ab78f8a3606154ab6fc9347fed
Harshad Nawathe <harshadn@thoughtworks.com>
2021-04-16 19:14:38 +0530

    Harshad - Use string template to greet the user.

--- src/main/kotlin/com/thoughtworks/counter/Application.kt
+++ src/main/kotlin/com/thoughtworks/counter/Application.kt
@@ -3,6 +3,6 @@ package com.thoughtworks.counter
 
 fun main() {
     println("Enter your name")
-    val line = readLine()
-    println(line)
+    val name = readLine()
+    println("Hello, $name")
 }
