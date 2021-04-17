e21149484af2f94af8692dc28c23fbbcf4e05087
Harshad Nawathe <harshadn@thoughtworks.com>
2021-04-17 10:20:37 +0530

    Harshad - Replace variable excitement by an expression in template

--- src/main/kotlin/com/thoughtworks/counter/Application.kt
+++ src/main/kotlin/com/thoughtworks/counter/Application.kt
@@ -4,6 +4,5 @@ package com.thoughtworks.counter
 fun main() {
     println("Enter your name")
     val name: String = readLine() ?: ""
-    val excitement = "!".repeat(name.length)
-    println("Hello $name$excitement")
+    println("Hello $name${"!".repeat(name.length)}")
 }
