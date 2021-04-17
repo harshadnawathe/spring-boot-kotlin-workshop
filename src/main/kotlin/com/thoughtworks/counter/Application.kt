9bfd4e0a2ac0160de285125384950f69a4204488
Harshad Nawathe <harshadn@thoughtworks.com>
2021-04-17 10:05:09 +0530

    Harshad - Use null check, if-else

--- src/main/kotlin/com/thoughtworks/counter/Application.kt
+++ src/main/kotlin/com/thoughtworks/counter/Application.kt
@@ -3,6 +3,10 @@ package com.thoughtworks.counter
 
 fun main() {
     println("Enter your name")
-    val name = readLine()
-    println("Hello, $name")
+    val name: String? = readLine()
+    if (null == name) {
+        println("Sorry, I didn't catch that.")
+    } else {
+        println("Hello, $name")
+    }
 }
